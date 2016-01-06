d4d4249b7602bf6705f782c16388aee40bab119c
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-06 15:34:00 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+var util = require('./util.js');
+var request = require('request').defaults({
+    baseUrl: 'https://api-ssl.bitly.com/'
+});
+
+var pickInputs = {
+        'domain': 'domain',
+        'unit': 'unit',
+        'units': 'units',
+        'timezone': 'timezone',
+        'limit': 'limit',
+        'unit_reference_ts': 'unit_reference_ts'
+    },
+    pickOutputs = {
+        'tz_offset': 'data.tz_offset',
+        'unit': 'data.unit',
+        'units': 'data.units',
+        'organization_clicks': 'data.organization_clicks'
+    };
+
 module.exports = {
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +27,25 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs),
+            token = dexter.environment('bitly_access_token'),
+            api = '/v3/organization/popular_links';
+
+        if (!token)
+            return this.fail('A [bitly_access_token] environment variable is required for this module');
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        inputs.access_token = token;
+        request.get({uri: api, qs: inputs, json: true}, function (error, response, body) {
+            if (error)
+                this.fail(error);
+            else if (body && body.status_code !== 200)
+                this.fail(body);
+            else
+                this.complete(util.pickOutputs(body, pickOutputs));
+        }.bind(this));
     }
 };
